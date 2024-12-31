7224833631bd7985570d61cf54646e43341d129a
mciiee <mrchingiz91@gmail.com>
2024-12-31 17:17:22 +0500

    fix: changed build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env sh
-(docker build -t worky:latest . && docker run --name worky -it -p 5002:5002 worky:latest) || (docker container stop worky && docker container rm worky)
+docker container stop worky && docker container rm worky
+docker build -t worky:latest . && docker run --name worky -d -p 5002:5002 worky:latest
+
