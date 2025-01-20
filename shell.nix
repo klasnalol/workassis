b2e4544bb2d1f5f13d4b32e201f72bdcd0a93350
mciiee <mrchingiz91@gmail.com>
2025-01-20 12:18:52 +0500

    dev(shell.nix): added ollama submodule dependencies

--- shell.nix
+++ shell.nix
@@ -1,9 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
+let ollamaDependencies = if builtins.pathExists ./ollama/dependencies.nix then (import ./ollama/dependencies.nix { inherit pkgs; }) else []; 
+in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
     python312
     python312Packages.python-lsp-server
-  ];
+  ] ++ ollamaDependencies;
   shellHook = ''
   '';  
 }
