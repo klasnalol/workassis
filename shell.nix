b2247eec169a640e6f97e401deaa057fff3c69bb
mciiee <mrchingiz91@gmail.com>
2025-01-22 13:24:47 +0500

    shell.nix: added more dependencies and patched a shell hook

--- shell.nix
+++ shell.nix
@@ -1,16 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
-let ollamaDependencies = if builtins.pathExists ./ollama/dependencies.nix then (import ./ollama/dependencies.nix { inherit pkgs; }) else []; 
+let 
+    ollamaDependencies = if builtins.pathExists ./ollama/dependencies.nix then (import ./ollama/dependencies.nix { inherit pkgs; }) else [];
+    devDependencies = with pkgs.buildPackages; [ python312Packages.python-lsp-server gnum4 nodejs_22];
+    pythonDependencies = with pkgs.buildPackages.python312Packages; [ sounddevice ];
 in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
-    python312
-    python312Packages.python-lsp-server
+    python312Full
     
-    gnum4
+    portaudio
 
-    nodejs_22
-  ] ++ ollamaDependencies;
+  ] ++ pythonDependencies ++ devDependencies ++ ollamaDependencies;
   shellHook = ''
-    m4 Makefile.m4 > Makefile
+    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib
   '';  
 }
