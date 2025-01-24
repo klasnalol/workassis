469e2e8807458900063875e3832e7e64e1497f10
mciiee <mrchingiz91@gmail.com>
2025-01-24 13:13:58 +0500

    routes: removed redundant routes/scripts.py

--- routes/scripts.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, send_from_directory
-
-scripts_bp = Blueprint('scripts', __name__)
-
-@scripts_bp.route('/scripts/<script>')
-def get_script(script):
-   return send_from_directory('static/scripts', script) 
