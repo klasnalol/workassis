57dd2808bd9ab758d2880186fdbd18fa870393ec
mciiee <mrchingiz91@gmail.com>
2025-01-27 13:31:49 +0500

    dev: added ffmpeg as a dev dependency and moved ollama/ submodule in to modules/ollama

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 let 
     ollamaDependencies = if builtins.pathExists ./ollama/dependencies.nix then (import ./ollama/dependencies.nix { inherit pkgs; }) else [];
-    devDependencies = with pkgs.buildPackages; [ python312Packages.python-lsp-server gnum4 nodejs_22];
+    devDependencies = with pkgs.buildPackages; [ python312Packages.python-lsp-server ffmpeg  gnum4 nodejs_22 ];
     pythonDependencies = with pkgs.buildPackages.python312Packages; [ sounddevice ];
 in
 pkgs.mkShell {
